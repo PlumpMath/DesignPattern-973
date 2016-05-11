168c6fd141da65264e139028dc38474f048df35d
Rounak <hasan.rounak@gmail.com>
2016-05-11 23:08:20 +0530

    Updated cutomer and Lead class

--- CustomerProjectPhase1/CustomerProjectPhase1/Class1.cs
+++ CustomerProjectPhase1/CustomerProjectPhase1/Class1.cs
@@ -6,17 +6,50 @@ using System.Threading.Tasks;
 
 namespace MiddleLayer
 {
-    public class Customer
-    {
+    public class CustomerBase
+        {
         public string CustomerName { get; set; }
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         public decimal BillAmount { get; set; }
         public DateTime BillDate { get; set; }
+        public virtual void Validate()
+            {
+            throw new NotImplementedException ();
+            }
         }
-    public class Lead
+    public class Customer: CustomerBase
         {
+        public override void Validate ( )
+            {
+            if (CustomerName.Length==0)
+                {
 
+                }
+            if (PhoneNumber.Length == 0)
+                {
+
+                }
+            if (Address.Length == 0)
+                {
+
+                }
+            if (CustomerName.Length == 0)
+                {
+
+                }
+            if (CustomerName.Length == 0)
+                {
+
+                }
+            }
+        }
+    public class Lead: CustomerBase
+        {
+        public override void Validate ( )
+            {
+            base.Validate ();
+            }
 
         }
     }
